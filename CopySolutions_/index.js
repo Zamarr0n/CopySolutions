54e7c70a99aee8f9152d86492c684627d31047d2
Emilio_Zamarron <93807309+Zamarr0n@users.noreply.github.com>
2023-03-14 15:23:32 -0600

    Add files via upload

---
+++ CopySolutions_/index.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else{
+
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden, .hidden1, .hidden2, .hidden3, .hidden4');
+hiddenElements.forEach((el) => observer.observe(el));
